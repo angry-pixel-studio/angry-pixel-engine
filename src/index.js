dcf822e5bc053ba24394a6e5e85d3b7c40186d5f
Mauro Cristy <maurocristy@gmail.com>
2020-09-09 19:08:57 -0300

    asdf

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Game from './Engine/Game';
+import Sandbox from './Demo/Scenes/Sandbox';
 import Race, { FINISHED_EVENT } from './Race/Scenes/Race';
 import raceData1 from "./Race/Test/race-result.json";
 import raceData2 from "./Race/Test/race-result-2.json";
@@ -7,17 +8,19 @@ const username = 'mauro';
 const containerElement = document.getElementById('app');
 
 // Create the Game
-const game = new Game(containerElement, 900, 502);
+//const game = new Game(containerElement, 900, 502);
+const game = new Game(containerElement, 1366, 768);
 
 // Add a scene
-game.addScene('Race', () => new Race(username, raceData2));
+//game.addScene('Race', () => new Race(username, raceData2));
+game.addScene('Race', () => new Sandbox());
 
 // Run the game
 game.run();
 
 // Stop the game
-window.addEventListener(FINISHED_EVENT, () => {
+/*window.addEventListener(FINISHED_EVENT, () => {
     console.log('carrera finalziada');
     
     setTimeout(() => game.stop(), 3000);
-});
+});*/
