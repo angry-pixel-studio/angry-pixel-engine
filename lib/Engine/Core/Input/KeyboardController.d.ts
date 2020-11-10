3528ff8e6795b81e42497464875f08c871155d77
Mauro Cristy <maurocristy@gmail.com>
2020-11-10 02:28:24 -0300

    improving keyboard controller

--- lib/Engine/Core/Input/KeyboardController.d.ts
+++ lib/Engine/Core/Input/KeyboardController.d.ts
@@ -1,5 +1,6 @@
 export declare class KeyboardController {
     private keyPresses;
     constructor();
+    private eventHandler;
     isPressed(key: string): boolean;
 }
--- src/Engine/Core/Input/KeyboardController.ts
+++ src/Engine/Core/Input/KeyboardController.ts
@@ -2,9 +2,19 @@ export class KeyboardController {
     private keyPresses: { [k: string]: boolean } = {};
 
     constructor() {
-        window.addEventListener("keydown", (e: KeyboardEvent) => (this.keyPresses[e.key] = true));
-        window.addEventListener("keyup", (e: KeyboardEvent) => (this.keyPresses[e.key] = false));
+        window.addEventListener("keydown", this.eventHandler);
+        window.addEventListener("keyup", this.eventHandler);
     }
+
+    private eventHandler = (event: KeyboardEvent) => {
+        if (event.type === "keydown") {
+            this.keyPresses[event.key] = true;
+        }
+
+        if (event.type === "keyup") {
+            this.keyPresses[event.key] = false;
+        }
+    };
 
     public isPressed(key: string): boolean {
         return this.keyPresses[key] !== undefined && this.keyPresses[key] !== false;
