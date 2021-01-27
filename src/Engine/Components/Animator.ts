43f8243cf16f9f7c436d1b88706e0f132c77d282
Mauro Cristy <maurocristy@gmail.com>
2021-01-27 01:16:06 -0300

    Anmator refactor

--- src/Engine/Components/Animator.ts
+++ src/Engine/Components/Animator.ts
@@ -4,7 +4,6 @@ import { Sprite } from "../Sprite";
 import { Animation } from "../Animation";
 import { TimeManager } from "../Core/Time/TimeManager";
 import { container } from "../Game";
-import { AnimationPlayer } from "./Animator/AnimationPlayer";
 
 interface Config {
     spriteRenderer: SpriteRenderer;
@@ -71,5 +70,44 @@ export class Animator extends Component {
         if (this.currentAnimation !== null) {
             this.currentAnimation = null;
         }
+    }
+}
+
+const FRAME_RATE: number = 24;
+
+class AnimationPlayer {
+    private animation: Animation;
+    private currentFrame: number = 0;
+    private frameTime: number = 0;
+    private _sprite: Sprite = null;
+    private _restarted: boolean = false;
+
+    constructor(animation: Animation) {
+        this.animation = animation;
+    }
+
+    public get sprite(): Sprite {
+        return this._sprite;
+    }
+
+    public get restarted(): boolean {
+        return this._restarted;
+    }
+
+    public get loop(): boolean {
+        return this.animation.loop;
+    }
+
+    public playFrame(deltaTime: number): void {
+        this._restarted = false;
+
+        if (this.frameTime >= 1 / (FRAME_RATE * this.animation.speed)) {
+            this.frameTime = 0;
+            this.currentFrame = this.currentFrame + 1 === this.animation.sprites.length ? 0 : this.currentFrame + 1;
+            this._restarted = this.currentFrame === 0;
+        }
+
+        this._sprite = this.animation.sprites[this.currentFrame];
+        this.frameTime += deltaTime;
     }
 }
