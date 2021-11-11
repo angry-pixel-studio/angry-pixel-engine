06aa15c55cdf2d1542346f98b38d557db004e0c9
Mauro Cristy <maurocristy@gmail.com>
2021-11-10 21:49:13 -0300

    ordering exports in index

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,26 @@ export * from "./math/Utils";
 export * from "./math/Rotation";
 
 // main
-export { Game, GameConfig, CollisionMethodConfig, Context2DConfig } from "./core/Game";
-export * from "./core/Scene";
-export * from "./core/GameObject";
 export { Component, PhysicsComponent, PreRenderComponent } from "./core/Component";
-export * from "./component/Sprite";
-export * from "./component/Animation";
-export * from "./component/renderingComponent/tilemap/Tileset";
+export { Game, GameConfig, CollisionMethodConfig, Context2DConfig } from "./core/Game";
+export * from "./core/GameObject";
+export * from "./core/Scene";
 
 // components
-export * from "./component/ComponentTypes";
 export * from "./component/colliderComponent/BoxCollider";
 export * from "./component/colliderComponent/TilemapCollider";
 export * from "./component/renderingComponent/SpriteRenderer";
 export * from "./component/renderingComponent/TextRenderer";
 export * from "./component/renderingComponent/tilemap/TiledTilemapRenderer";
 export * from "./component/renderingComponent/tilemap/CsvTilemapRenderer";
+export * from "./component/renderingComponent/tilemap/Tileset";
+export * from "./component/Animation";
 export * from "./component/Animator";
 export * from "./component/AudioPlayer";
 export * from "./component/Camera";
+export * from "./component/ComponentTypes";
 export * from "./component/RigidBody";
+export * from "./component/Sprite";
 export * from "./component/Transform";
 
 // game objects
@@ -32,20 +32,20 @@ export * from "./gameObject/GameCamera";
 export * from "./gameObject/SpacePointer";
 
 // facades
-export { SceneManagerFacade as SceneManager } from "./core/facades/SceneManagerFacade";
-export { InputManagerFacade as InputManager } from "./core/facades/InputManagerFacade";
 export { AssetManagerFacade as AssetManager } from "./core/facades/AssetManagerFacade";
 export { DomManagerFacade as DomManager } from "./core/facades/DomManagerFacade";
-export { TimeManagerFacade as TimeManager } from "./core/facades/TimeManagerFacade";
 export { GameObjectManagerFacade as GameObjectManager } from "./core/facades/GameObjectManagerFacade";
+export { InputManagerFacade as InputManager } from "./core/facades/InputManagerFacade";
+export { SceneManagerFacade as SceneManager } from "./core/facades/SceneManagerFacade";
+export { TimeManagerFacade as TimeManager } from "./core/facades/TimeManagerFacade";
 
 // input
+export * from "./input/GamepadController";
 export * from "./input/KeyboardController";
 export * from "./input/MouseController";
-export * from "./input/GamepadController";
 export * from "./input/TouchController";
 
-// Others
+// others
 export { Collision } from "./physics/collision/CollisionManager";
 export { ICollider } from "./physics/collision/collider/ICollider";
 export { CollisionData } from "./physics/collision/CollisionData";
