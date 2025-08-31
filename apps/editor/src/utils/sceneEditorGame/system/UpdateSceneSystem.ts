a7ae2d47ab9d0947a9ed5e68a641bf68b29cbd99
Mauro Cristy <maurocristy@gmail.com>
2025-08-31 09:14:23 -0300

    reading component change state from system

--- apps/editor/src/utils/sceneEditorGame/system/UpdateSceneSystem.ts
+++ apps/editor/src/utils/sceneEditorGame/system/UpdateSceneSystem.ts
@@ -2,12 +2,13 @@ import { AssetManager, EntityManager, inject, SYMBOLS, System, Transform } from 
 import { SceneState } from "../../../stores/sceneStore";
 import { UseBoundStore, StoreApi } from "zustand";
 import { EntityIdentifier } from "../component/EntityIdentifier";
+import { getComponentType } from "../utils/components";
+import { BuiltInComponent } from "../../../types/component";
 
 export class UpdateSceneSystem implements System {
     private unsubscribe: () => void = () => {};
     private stateHasChanged: boolean = false;
     private currentState: SceneState | undefined;
-    private prevState: SceneState | undefined;
 
     constructor(
         @inject(SYMBOLS.EntityManager) private readonly entityManager: EntityManager,
@@ -16,29 +17,27 @@ export class UpdateSceneSystem implements System {
     ) {}
 
     onEnabled(): void {
-        this.unsubscribe = this.useSceneStore.subscribe((state: SceneState, prevState: SceneState) => {
+        this.unsubscribe = this.useSceneStore.subscribe((state: SceneState) => {
             this.stateHasChanged = true;
             this.currentState = state;
-            this.prevState = prevState;
         });
     }
 
     onUpdate(): void {
         if (this.stateHasChanged && this.currentState) {
-            console.log("stateHasChanged", this.currentState.entityUpdated, this.currentState.componentUpdated);
             if (this.currentState.componentUpdated) {
                 const [entityId, componentId] = this.currentState.componentUpdated;
                 const componentData = this.currentState.componentsMap
                     .get(entityId)
                     ?.find((comp) => comp.id === componentId);
                 if (componentData) {
-                    const entity = this.entityManager.search(EntityIdentifier, (comp) => comp.id === entityId)[0]
-                        .entity;
-
-                    // TODO: pick the propper component type based on the componentData.name
-                    this.entityManager.updateComponentData(entity, Transform, (component) => {
-                        Object.assign(component, componentData.data);
-                    });
+                    const { entity } = this.entityManager.search(EntityIdentifier, (comp) => comp.id === entityId)[0];
+                    const componentType = getComponentType(componentData.name as BuiltInComponent);
+                    if (componentType) {
+                        this.entityManager.updateComponentData(entity, componentType, (component) => {
+                            Object.assign(component, componentData.data);
+                        });
+                    }
                 }
             }
             this.stateHasChanged = false;
