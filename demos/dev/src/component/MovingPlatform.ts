d709e0c407245f43c72d59d791d6df3d3d9dcde5
Mauro Cristy <maurocristy@gmail.com>
2026-08-15 14:12:43 -0300

    chore: create the dev demo entities from the tilemap

--- demos/dev/src/component/MovingPlatform.ts
+++ demos/dev/src/component/MovingPlatform.ts
@@ -1,8 +1,9 @@
-import { Vector2 } from "angry-pixel";
+import { Entity, Vector2 } from "angry-pixel";
 
 export class MovingPlatform {
     public direction: Vector2 = new Vector2();
     public nextSpot: number = 0;
+    public spotEntities: Entity[] = [];
     public spots: Vector2[] = [];
     public speed: number = 50;
 
