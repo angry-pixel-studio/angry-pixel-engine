c3231ab87c64551574d25d6a46ba255d48239408
Mauro Cristy <maurocristy@gmail.com>
2026-09-23 23:13:05 -0300

    fix: cache the light uniform locations

--- packages/webgl/src/renderer/DarknessRenderer.ts
+++ packages/webgl/src/renderer/DarknessRenderer.ts
@@ -1,7 +1,7 @@
 import { mat4 } from "gl-matrix";
 import { CameraData, RenderData, RenderDataType, Renderer } from "./Renderer";
 import { Vector2 } from "@angry-pixel/math";
-import { ProgramManager } from "../program/ProgramManager";
+import { MAX_LIGHTS, ProgramManager } from "../program/ProgramManager";
 import { hexToRgba, setProjectionMatrix } from "./utils";
 
 export interface DarknessRenderData extends RenderData {
@@ -93,31 +93,21 @@ export class DarknessRenderer implements Renderer {
         this.gl.uniform4f(this.programManager.solidColorUniform, r, g, b, a);
         this.gl.uniform1f(this.programManager.alphaUniform, renderData.opacity ?? 1);
 
-        this.gl.uniform1i(this.programManager.numLightsUniform, renderData.lights.length);
+        this.gl.uniform1i(this.programManager.numLightsUniform, Math.min(renderData.lights.length, MAX_LIGHTS));
 
         for (const [i, light] of renderData.lights.entries()) {
-            if (i >= 64) break; // 64 is the max lights allowed
+            if (i >= MAX_LIGHTS) break;
+
+            const uniforms = this.programManager.lightUniforms[i];
 
             this.gl.uniform2f(
-                this.gl.getUniformLocation(this.programManager.program, `u_lights[${i}].position`),
+                uniforms.position,
                 this.gl.canvas.width / 2 + (light.position.x - cameraData.position.x) * cameraData.zoom,
                 this.gl.canvas.height / 2 + (light.position.y - cameraData.position.y) * cameraData.zoom,
             );
-
-            this.gl.uniform1f(
-                this.gl.getUniformLocation(this.programManager.program, `u_lights[${i}].radius`),
-                light.radius * cameraData.zoom,
-            );
-
-            this.gl.uniform1f(
-                this.gl.getUniformLocation(this.programManager.program, `u_lights[${i}].smoothMode`),
-                light.smooth ? 1 : 0,
-            );
-
-            this.gl.uniform1f(
-                this.gl.getUniformLocation(this.programManager.program, `u_lights[${i}].intensity`),
-                Math.max(0, Math.min(1, light.intensity)),
-            );
+            this.gl.uniform1f(uniforms.radius, light.radius * cameraData.zoom);
+            this.gl.uniform1f(uniforms.smoothMode, light.smooth ? 1 : 0);
+            this.gl.uniform1f(uniforms.intensity, Math.max(0, Math.min(1, light.intensity)));
         }
 
         this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
